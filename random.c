059a1e2b732c23ff252a89692221256ab1f0683e
Philip Puryear <philippuryear@gmail.com>
2014-04-20 21:19:52 -0500

    Various minor improvements.

--- random.c
+++ random.c
@@ -1,4 +1,4 @@
-// Copyright 2012 Philip Puryear
+// Copyright 2014 Philip Puryear
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,12 +13,13 @@
 // limitations under the License.
 
 #include <errno.h>
+#include <fcntl.h>
 #include <limits.h>
 #include <stdarg.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <getopt.h>
 #include <gmp.h>
 #include "simple_strconv.h"
@@ -26,11 +27,11 @@
 static const int kDefaultBase = 10;
 static const unsigned long kDefaultUpperBound = 1UL << 15;
 static const int kMaxReads = 100;
-static const char *gRandomFileName = "/dev/urandom";
+static const char *gRNGFilename = "/dev/urandom";
 
 static void fatal(const char *msg, ...) {
     va_list ap;
-    fprintf(stderr, "random: fatal: ");
+    fprintf(stderr, "random: error: ");
     va_start(ap, msg);
     vfprintf(stderr, msg, ap);
     va_end(ap);
@@ -57,93 +58,88 @@ static void arg_to_mpz(mpz_t result, const char *arg) {
 }
 
 static void get_random_mpz(mpz_t result, mpz_t low, mpz_t high) {
-    // In the comments below, let L be the length of the desired interval,
-    // i.e. |high - low|.
-    // To conserve memory, repurpose |high| to be a temporary.
-    mpz_sub(high, high, low);
-    if (mpz_sgn(high) <= 0)
-        fatal("upper bound must be strictly greater than lower bound");
+    mpz_t range;
+    mpz_init(range);
+    mpz_sub(range, high, low);
+    if (mpz_sgn(range) <= 0)
+        fatal("upper bound must be greater than lower bound");
 
-    // If L == 1, there's only one possible result, so return it.
-    mpz_sub_ui(high, high, 1);
-    if (mpz_sgn(high) == 0) {
+    // If range == 1, there's only one possible result, so return it.
+    mpz_sub_ui(range, range, 1);
+    if (mpz_sgn(range) == 0) {
         mpz_set(result, low);
         return;
     }
 
-    // The system PRNG provides us with a source of of random *bits*, so we
-    // need to figure out the number of bits required to represent L.
-    size_t num_bits = mpz_sizeinbase(high, 2);
+    // Allocate a buffer to hold the RNG data.
+    size_t num_bits = mpz_sizeinbase(range, 2);
     size_t num_bytes = 1 + (num_bits - 1) / CHAR_BIT;
-    char *random_bytes = (char*) malloc(num_bytes);
-    if (!random_bytes)
+    char *random_data = malloc(num_bytes);
+    if (!random_data)
         fatal("out of memory");
 
-    FILE *random_file = fopen(gRandomFileName, "r");
-    if (!random_file)
-        fatal("could not open %s: %s", gRandomFileName, strerror(errno));
-    // Turn off buffering to avoid reading (and hence making the system
-    // generate) more random data than we need.
-    setbuf(random_file, NULL);
+    int rng_fd = open(gRNGFilename, O_RDONLY);
+    if (rng_fd < 0)
+        fatal("could not open %s: %s", gRNGFilename, strerror(errno));
 
     // Read a number R in the interval [0, 2^|num_bits|) from the RNG.
-    // Note that if L is not a power of 2, then L < 2^|num_bits|, so R may be
-    // >= L by *at most* a factor of 2. If this happens, retry until R < L.
+    // Note that if the range L is not a power of 2, then L < 2^|num_bits|, so
+    // R may be >= L by at most a factor of 2. If this happens, retry until
+    // R < L.
     //
     // Strictly speaking, there is a chance that no matter how many times we
     // read, R will be >= L each time, so cap the attempts at some reasonable
     // value. For a cap of N, the chance that we'll never read a valid number
     // is at most 1/2^N, which for N=100 is less than one in a nonillion.
-    int reads = 0;
-    while (1) {
-        if (fread(random_bytes, 1, num_bytes, random_file) != num_bytes)
-            fatal("error reading from %s", gRandomFileName);
+    int num_reads = 0;
+    do {
+        ssize_t num_bytes_read = read(rng_fd, random_data, num_bytes);
+        if (num_bytes_read != num_bytes)
+            fatal("error reading from %s: %s", gRNGFilename,
+                num_bytes_read < 0 ? strerror(errno) : "not enough bytes read");
+
         // Chop off unnecessary leading bits.
         if (num_bits % CHAR_BIT != 0)
-            random_bytes[0] &= (1 << (num_bits % CHAR_BIT)) - 1;
+            random_data[0] &= (1 << (num_bits % CHAR_BIT)) - 1;
 
-        mpz_import(result, num_bytes, 1, 1, 0, 0, random_bytes);
-        // At this point, |high| == L - 1, so we need a <= here.
-        if (mpz_cmp(result, high) <= 0)
-            break;
+        mpz_import(result, num_bytes, 1, 1, 0, 0, random_data);
+    } while (mpz_cmp(result, range) > 0 && ++num_reads < kMaxReads);
+    if (num_reads == kMaxReads)
+        fatal("system did not return a number within the given bounds");
 
-        if (++reads == kMaxReads)
-            fatal("system did not return a number within the given bounds");
-    }
     mpz_add(result, result, low);
-    fclose(random_file);
-    free(random_bytes);
+    close(rng_fd);
+    free(random_data);
+    mpz_clear(range);
 }
 
 int main(int argc, char **argv) {
-    bool use_bits = false;
-    mp_bitcnt_t bits;
+    mp_bitcnt_t bit_width = 0;
     int base = kDefaultBase;
 
     static const struct option long_options[] = {
         { "help", no_argument, NULL, 'h' },
         { "use-random", no_argument, NULL, 'r' },
         { "base", required_argument, NULL, 'b' },
-        { "size", required_argument, NULL, 's' },
+        { "bit-width", required_argument, NULL, 's' },
         { NULL, 0, NULL, 0 }
     };
-    while (1) {
-        int c = getopt_long(argc, argv, "hrb:s:", long_options, NULL);
-        if (c == -1)
+    for (;;) {
+        int opt = getopt_long(argc, argv, "hrb:s:", long_options, NULL);
+        if (opt == -1)
             break;
 
-        switch (c) {
+        switch (opt) {
         case 'r':
-            gRandomFileName = "/dev/random";
+            gRNGFilename = "/dev/random";
             break;
         case 'b':
             if (simple_strtoi(&base, optarg, 10) < 0 ||
-                abs(base) < 2 || base < -36 || base > 62)
+                (base > -2 && base < 2) || base < -36 || base > 62)
                 fatal("invalid base: '%s'", optarg);
             break;
         case 's':
-            use_bits = true;
-            if (simple_strtoul(&bits, optarg, 10) < 0)
+            if (simple_strtoul(&bit_width, optarg, 10) < 0 || bit_width == 0)
                 fatal("invalid bit width: '%s'", optarg);
             break;
         case 'h':
@@ -155,7 +151,7 @@ int main(int argc, char **argv) {
     }
     argv += optind;
     argc -= optind;
-    if (argc > 2 || (use_bits && argc > 0))
+    if (argc > 2 || (bit_width != 0 && argc > 0))
         fatal("too many arguments");
 
     mpz_t low, high, result;
@@ -165,8 +161,8 @@ int main(int argc, char **argv) {
         arg_to_mpz(high, argv[1]);
     } else if (argc == 1) {
         arg_to_mpz(high, argv[0]);
-    } else if (use_bits) {
-        mpz_setbit(high, bits);
+    } else if (bit_width != 0) {
+        mpz_setbit(high, bit_width);
     } else {
         mpz_set_ui(high, kDefaultUpperBound);
     }
